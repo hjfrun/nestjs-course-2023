71630c1a298ae3e91e5fb5a5d5c9371f83e5f047
hjfrun <hjfrun@gmail.com>
2023-03-21 17:08:11 +0800

    finish user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,15 @@
-import { Body, Controller, Get, Post } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post
+} from '@nestjs/common'
 import { CreateUserDto } from './dto/create-user.dto'
+import { UpdateUserDto } from './dto/update-user.dto'
 import { UserService } from './user.service'
 
 @Controller('user')
@@ -7,11 +17,29 @@ export class UserController {
   constructor(private userService: UserService) {}
   @Get()
   getUsers() {
-    return this.userService.get()
+    return this.userService.getAll()
+  }
+
+  @Get(':id')
+  getUser(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.getOne(id)
   }
 
   @Post()
   store(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto)
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateUserDto: UpdateUserDto
+  ) {
+    return this.userService.update(id, updateUserDto)
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.delete(id)
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 import { CreateUserDto } from './dto/create-user.dto'
+import { UpdateUserDto } from './dto/update-user.dto'
 import { User } from './entity/user.entity'
 
 @Injectable()
@@ -10,11 +11,24 @@ export class UserService {
     @InjectRepository(User)
     private userRepository: Repository<User>
   ) {}
-  get() {
+
+  getAll() {
     return this.userRepository.find()
+  }
+
+  getOne(id: number) {
+    return this.userRepository.findOne({ where: { id } })
   }
 
   create(createUserDto: CreateUserDto) {
     return this.userRepository.save(createUserDto)
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return this.userRepository.update(id, updateUserDto)
+  }
+
+  delete(id: number) {
+    return this.userRepository.delete(id)
   }
 }
